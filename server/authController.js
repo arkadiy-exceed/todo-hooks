843c666fe5628246d079474b1b90d1ef7f5747e6
Arkadiy <arzumanov.exceedteam@gmail.com>
2021-09-16 11:51:35 +0300

    regist and auth, authRouter and  authController

--- server/authController.js
+++ server/authController.js
@@ -1,17 +1,61 @@
 const User = require('./model/User.js');
+const bcrypt = require('bcrypt');
 
 class authController {
     async registration(req, res) {
+        const {email, username, password} = req.body;
+        console.log(req.body)
         try {
+            const userEmail = await User.findOne({
+                email: email
+            })
+            const userName = await User.findOne({
+                username: username
+            })
+            if (userEmail) {
+                res.send({
+                    error: 'existing email'
+                }) 
+            } else if (userName) {
+                res.send({
+                    error: 'existing username'
+                })
+            } else {
+                const hashPassword = bcrypt.hashSync(password, 7);
+                const user = await User.create({
+                    email: email,
+                    username: username,
+                    password: hashPassword
+                })
 
+                res.send(user)
+                console.log(user)
+            }
         } catch(e) {
 
         }
     }
 
     async login(req, res) {
+        const {username, password} = req.body;
         try {
-
+            const user = await User.findOne({
+                username: username
+            })
+            if (!user) {
+                res.send({
+                    error: 'user doesnt exist'
+                })
+            }
+            const validPassword = bcrypt.compareSync(password, user.password)
+            if(!validPassword) {
+                res.send({
+                    error: 'invalid password'
+                })
+                console.log(validPassword)
+            } else {
+                res.send('success')
+            }
         } catch(e) {
             
         }
--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,25 +1,66 @@
-// import { useState } from 'react';
+import { useState } from 'react';
+import axios from "axios";
 import './Auth.css';
 
 function Auth() {
+    const [usernameInput, setUsernameInput] = useState('');
+    const [passInput, setPassInput] = useState('');
+    const [usernameError, setUsernameError] = useState('');
+    const [passError, setPassError] = useState('');
+
+    const submitForm = (e) => {
+        e.preventDefault();
+
+        axios.post('http://localhost:3002/auth/login', {
+            username: usernameInput,
+            password: passInput
+        })
+        .then(res => {
+            if (res.data.error === 'user doesnt exist') {
+                setUsernameError(res.data.error);
+                setPassError('');
+            } else if (res.data.error === 'invalid password') {
+                setPassError(res.data.error);
+                setUsernameError('')
+            } else {
+                console.log(res.data);
+                setUsernameError('');
+                setPassError('');
+            }
+            console.log(res);
+        })
+    }
 
     return (
         <div>
             <h2>sign in</h2>
 
-            <form className='form'>
+            <form 
+                className='form'
+                onSubmit={submitForm}
+            >
 
                 <input
                     className='form__input'
                     placeholder='your username'
                     type='text'
+                    onChange={(e) => setUsernameInput(e.target.value)}
                 />
+
+                <div>
+                    {usernameError}
+                </div>
 
                 <input
                     className='form__input'
                     placeholder='your password'
                     type='password'
+                    onChange={(e) => setPassInput(e.target.value)}
                 />
+
+                <div>
+                    {passError}
+                </div>
 
                 <button
                     className='form__btn'
